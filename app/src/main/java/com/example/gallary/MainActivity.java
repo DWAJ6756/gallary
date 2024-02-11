c4f69a7535f88be06c94ca1c02f457378c8a7030
Shreedwaj <shreedwajdjs5@gmail.com>
2024-02-11 22:34:49 +0530

    first build

---
+++ app/src/main/java/com/example/gallary/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.gallary;
+
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.ImageView;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView image;
+    Button btn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image= findViewById(R.id.image);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri= data.getData();
+        image.setImageURI(uri);
+    }
+}
